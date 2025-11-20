78622678815cf1c5eb61c9e82e0682b1f946203b
KhoaND1307 <khoa130704@gmail.com>
2025-11-21 00:41:15 +0700

    fix: backend deployment issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || 3000;
+  if (!port) {
+    // This is the cleanest way to fail if the environment is misconfigured.
+    throw new Error(
+      'PORT environment variable is required and must be defined.',
+    );
+  }
+
+  await app.listen(port);
 }
 bootstrap();
