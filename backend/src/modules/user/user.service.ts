d533896c535d132ebf03ee1b1c58a3ad64b036ee
huy <trgiahuy2906@gmail.com>
2025-11-22 17:15:46 +0700

    feat:getTree

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@db/services';
-import { User, Prisma } from '@prisma/client';
+import { User, Prisma } from '../../../generated/client';
 
 @Injectable()
 export class UserService {
