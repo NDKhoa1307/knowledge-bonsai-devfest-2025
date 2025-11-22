dde05ceb9227d75828df24e7ed88f093aa472c36
KhoaND1307 <khoa130704@gmail.com>
2025-11-22 17:18:26 +0700

    fix: enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT || 8081;
+  // Enable CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
   if (!port) {
     // This is the cleanest way to fail if the environment is misconfigured.
     throw new Error(
