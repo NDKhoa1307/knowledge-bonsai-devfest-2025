01ccd35e1acd9e6a98a51da9ad935243181902a7
KhoaND1307 <khoa130704@gmail.com>
2025-11-22 22:23:25 +0700

    feat: update create tree module

--- backend/src/modules/tree/prompts/createTree.prompts.ts
+++ backend/src/modules/tree/prompts/createTree.prompts.ts
@@ -5,5 +5,8 @@ which can help a user with a certain topic. You will be given
 a topic, your task is to generate a detailed knowledge tree based on the user's prompt.
 The tree should have a clear hierarchy with nodes representing
 key concepts, subtopics, and details. Each node should have
-an id, label, type (pot, trunk, leaf), level, and optional children.
+an id, label, type (pot, branch, trunk, leaf,...), level, and optional children.
+
+IMPORTANT: Make sure to create at most 3 leaves per branch. Make sure to also
+include all node types.
 `;
